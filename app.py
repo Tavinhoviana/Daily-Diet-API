a98e7c7809cf5e41e8273356e2a3524e43af948c
Your Name <you@example.com>
2025-05-15 07:31:53 -0300

    updating readme, app, and creating models users

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "your_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///database.db"
 
-@app.route("/")
+db = SQLAlchemy(app)
+
+@app.route("/hello world", methods=["GET"])
 def hello_world():
     return "hello world"
-
-@app.route("/about")
-def about():
-    return "pagina sobre"
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ models/users.py
@@ -0,0 +1,4 @@
+from app import db
+
+class User(db.Model):
+    # id (int), username (txt), description (txt), date and time, (int), role (txt), 
