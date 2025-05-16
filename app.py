d5efc45163325a9d47e89bcfda6d107a80cf14f9
Your Name <you@example.com>
2025-05-16 09:04:32 -0300

    Implementing user roles

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager, login_user
+from flask_login import LoginManager, login_user, current_user, logout_user, login_required
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "your_secret_key"
@@ -11,15 +11,10 @@ login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
 
-with app.app_context():
-    db.create_all()
 # View login
+login_manager.login_view = "login"
 
 # Session <- conexao ativa
-
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
 
 @app.route("/register", methods=["POST"])
 def register():
@@ -28,10 +23,13 @@ def register():
     password = data.get("password")
 
     if not username or not password:
-        return jsonify({"error": "Informe usuário e senha"}), 400
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Informe usuário e senha"}), 400
 
     if User.query.filter_by(username=username).first():
-        return jsonify({"error": "Usuário já existe"}), 400
+        return jsonify({"message": "Usuário já existe"}), 400
 
     new_user = User(username=username, password=password)
     db.session.add(new_user)
@@ -39,22 +37,70 @@ def register():
 
     return jsonify({"message": "Usuário registrado com sucesso"}), 201
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 @app.route("/login", methods=["POST"])
 def login():
     data = request.json
     username = data.get("username")
     password = data.get("password")
 
-    if not username or not password:
-        return jsonify({"message": "Informe usuário e senha"}), 400
+    if username and password:
+        user = User.query.filter_by(username=username).first()
 
-    user = User.query.filter_by(username=username).first()
+        if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)  # Corrigido
+            return jsonify({"message": "Autenticação realizada com sucesso"})
+
+    return jsonify({"message": "Credenciais inválidas"}), 400
+
+@app.route("/logout", methods=["GET"])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout realizado com sucesso"})
+
+@app.route("/user/<int:id_user>", methods=["GET"])
+@login_required
+def read_user(id_user):
+    user = User.query.get(id_user)
 
     if user:
-        login_user(user)
-        return jsonify({"message": f"Bem-vindo, {user.username}!"})
-    else:
-        return jsonify({"message": "Usuário não encontrado"}), 404
+        return {"username": user.username}
+    
+    return jsonify ({"message": "Usuario nao encontrado"}), 404
+
+    
+@app.route("/user/<int:id_user>", methods=["PUT"])
+@login_required
+def update_user(id_user):
+    data = request.json
+    user = User.query.get(id_user)
+
+    if user and data.get("password"):
+        user.password = data.get("password")
+        db.session.commit()
+        return jsonify ({"message": f"Usuário {id_user} atualizado com sucesso"})
+    
+    return jsonify ({"message": "Usuario nao encontrado"}), 404 
+
+@app.route("/user/<int:id_user>", methods=["DELETE"])
+@login_required
+def delete_user(id_user):
+    user = User.query.get(id_user)
+
+    if id_user == current_user.id:
+        return jsonify ({"message": "Deleçao nao permitida"}), 403
+    
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify ({"message": "Usuario deletado com sucesso"})
+    
+    return jsonify ({"message": "Usuario nao encontrado"}), 404 
 
 if __name__ == "__main__":
     app.run(debug=True, port=5050)
